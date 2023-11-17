e637f1837cddd12c454d7eefa5df5da69e32fab0
Igor-ZA <Igor-ZA@users.noreply.github.com>
2023-11-17 08:34:46 +0100

    Init

---
+++ src/MauiApp9/MobileDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection;
+
+namespace MauiApp9
+{
+    public class MobileDbContext(DbContextOptions dbOptions) : DbContext(dbOptions)
+    {
+        public DbSet<ProjectEntity> Projects { get; set; }
+
+        protected sealed override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    public class ProjectEntity
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class ProjectConfig : IEntityTypeConfiguration<ProjectEntity>
+    {
+        public void Configure(EntityTypeBuilder<ProjectEntity> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(m => m.Name).HasMaxLength(120);
+        }
+    }
+}
---
+++ src/MauiApp9/Platforms/Android/MainApplication.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace MauiApp9
+{
+    [Application]
+    public class MainApplication(IntPtr handle, JniHandleOwnership ownership) : MauiApplication(handle, ownership)
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+
+            using var scope = IPlatformApplication.Current.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<MobileDbContext>();
+            context.Database.EnsureCreated();
+        }
+    }
+}
